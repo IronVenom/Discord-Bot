3879a85a8084d36ed5f343c61105c2e7c4d04fc3
Atharva Kulkarni <kulkarniatharva235@gmail.com>
2019-04-17 11:47:42 +0530

    Updated the help commands

--- Sparky.py
+++ Sparky.py
@@ -133,7 +133,7 @@ async def on_message(message):
 		embed = discord.Embed(title='Topic Based Roles Help',description='Machine Learning/ Artificial Intelligence/ Internet of Things/ Cyber Security',colour=discord.Colour.purple())
 		embed.add_field(name='topicrole! name of role from above 4',value='Adds the role',inline=False)
 		embed.add_field(name='topicroleremove! removes role from above 4',value='Removes the role',inline=False)
-		embed.add_field(name='Example',value = 'topicrole! machine learning\ntopicrolermove! machine learning\nAlso note that the commands are case insensitive.',inline = False)
+		embed.add_field(name='Example',value = 'topicrole! machine learning\ntopicroleremove! machine learning\nAlso note that the commands are case insensitive.',inline = False)
 		await client.send_message(message.channel,embed=embed)
 
 		
