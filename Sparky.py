55676dbd62515e857004de8724e334011874345d
Atharva Kulkarni <kulkarniatharva235@gmail.com>
2019-09-01 07:29:23 +0530

    Update Sparky.py

--- Sparky.py
+++ Sparky.py
@@ -872,20 +872,12 @@ async def on_message(message):
 
 	if message.content.upper().startswith('PSRULES!'):
 			
-		channel_CP = client.get_channel(os.getenv('CP_CHANNEL_ID'))
-		role_id_list=[]
-		for role in message.server.roles:
-			if role.name.upper() == 'HELP STAFF':
-				role_id_list.append(role.mention)
 		embed = discord.Embed(title='Practice Session Rules',description='To be followed by everyone who is participating',colour=discord.Colour.red())
 		embed.add_field(name='Rule-1',value='Post your solutions in the practice sessions channel using appropriate discord markdown.',inline='False')
-		embed.add_field(name='Rule-2',value='If you have a doubt, ping anyone of the support staff mentioned below. Don\'t ping the entire role',inline='False')
-		embed.add_field(name='Rule-3',value='Try to make your code as efficient as possible. If you don\'t know about efficiency, leave this point.',inline='False')
+		embed.add_field(name='Rule-2',value='If you have a doubt, ping anyone of the General Help Staff who are online. Don\'t ping the entire role',inline='False')
+		embed.add_field(name='Rule-3',value='Try to make your code as efficient as possible.',inline='False')
 		embed.add_field(name='Rule-4',value='Do not cheat or copy.',inline='False')
-		embed.add_field(name='Rule-5',value='Use logic along with the in-built functions to get the most output.',inline='False')
-		embed.add_field(name='Rule-6',value='Use C++ / C /Python / Java. If you feel excited, use Haskell or Erlang at your own risk.',inline='False')
 		embed.add_field(name='Link for Discord Markup',value='https://support.discordapp.com/hc/en-us/articles/210298617-Markdown-Text-101-Chat-Formatting-Bold-Italic-Underline-',inline='False')
-		embed.add_field(name='Support Staff',value=role_id_list[0],inline='False')
 		await client.send_message(message.channel,embed=embed)
 			
 	#PING
